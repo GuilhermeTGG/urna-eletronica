9d94f41eb08fa339c3b4431c895c37a1bc97e99c
GuilhermeTGG <guilherme050592@gmail.com>
2022-09-06 09:03:38 -0300

    Projeto urna

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: "DEPUTADO FEDERAL",
+        numeros: 4,
+        candidatos: [
+        {
+            numero: "2234",
+            nome: "MARK O ZUCKE",
+            partido: "NRD",
+            fotos: [
+                {url:'55555.jpg', legenda:'Deputado Federal'}
+            ]
+        },
+        {
+            numero: "4456",
+            nome: "MUSK O ELON",
+            partido: "SPX",
+            fotos: [
+                {url:'88888.jpg', legenda:'Deputado Federal'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: "PRESIDENTE",
+        numeros: 2,
+        candidatos: [
+        {
+            numero: "17",
+            nome: "BIROLIRO",
+            partido: 'LLL',
+            vice: "Bozoloro",
+            fotos: [
+                {url:'17.jpg', legenda:'Presidente'},
+                {url:'420.jpg', legenda: 'Vice-Presidente', small: true}
+            ]
+        },
+        {
+            numero: "13",
+            nome: "LULALAU",
+            partido: 'FPT',
+            vice: "Tiriroca",
+            fotos: [
+                {url:'13.jpg', legenda:'Presidente'},
+                {url:'155.jpg', legenda:'Vice-Presidente', small: true}
+            ]
+        }
+        ]
+    }
+];
